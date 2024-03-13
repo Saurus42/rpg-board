6b84cea77705e1eac85f7e0bf06d633f93963cca
Saurus42 <satoschi358@gmail.com>
2024-03-13 12:11:29 +0100

    Update script.

--- build.py
+++ build.py
@@ -5,11 +5,23 @@ if shutil.which( "cargo" ) == None:
     print( "Please install cargo." )
     exit( 0 )
 
+builder = {
+    "linux": "x86_64-unknown-linux-gnu",
+    "windows": "x86_64-pc-windows-msvc",
+    "rpi-linux": "aarch64-unknown-linux-gnu"
+}
+
 try:
-    print( "Build application. Please await." )
-    args = ""
-    for index in range( 1, len( sys.argv ) ):
-        args += sys.argv[index] + " "
+    platform = ""
+    target = ""
+    for plat in sys.argv:
+        data = plat.split( "=" )
+        if data[0] == "platform":
+            platform = "--target=" + builder[data[1]]
+        if data[0] == "target":
+            target = "--" + data[1]
+    print( "Application is building. Please await." )
+    args = f"{platform} {target}"
     subprocess.check_output( f"cargo build {args}" )
 except subprocess.CalledProcessError as error:
     print( error.output )
